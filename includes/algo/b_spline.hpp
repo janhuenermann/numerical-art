d80a7e70dc2647ed230f2b58f8c54fbb1e4ca82a
Jan Hünermann <jan@huenermann.de>
2020-03-21 17:07:40 +0800

    Add comments

--- includes/algo/b_spline.hpp
+++ includes/algo/b_spline.hpp
@@ -18,11 +18,30 @@ struct BSpline
     vector<double> knots;
     vector<VectorNd> control_points;
 
+    /**
+     * Interpolates a point on the spline. Supply k to be faster.
+     *
+     * Algorithm from https://en.wikipedia.org/wiki/De_Boor%27s_algorithm.
+     * @param  tau Value in the support of the spline.
+     * @return     Point on spline.
+     */
     VectorNd interpolate(double tau) const;
     VectorNd interpolate(double tau, int k) const;
 
+    /**
+     * Walks the spline.
+     *
+     * @param delta  Resolution
+     * @param fn     Callback
+     */
     void walk(double delta, void (*fn)(VectorNd pt, const BSpline &)) const;
 
+    /**
+     * Creates a spline from knots (numbers in support), and control points.
+     * @param  knots          f(knots) = control_points. These are knot points.
+     * @param  control_points The vectors at each knot point.
+     * @return                BSpline
+     */
     static BSpline create(vector<double> knots, vector<VectorNd> control_points);
 
 };
